500f08b392175508b6c9c662d367c7ebe24b7baa
gpontecorvo <gregoryp@lynden.com>
2021-11-30 14:14:08 -0800

    Mofidy topic names for demo

--- apache-kafka/src/test/java/com/baeldung/kafkastreams/KafkaStreamsLiveTest.java
+++ apache-kafka/src/test/java/com/baeldung/kafkastreams/KafkaStreamsLiveTest.java
@@ -1,13 +1,5 @@
 package com.baeldung.kafkastreams;
 
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.Properties;
-import java.util.regex.Pattern;
-
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
@@ -17,18 +9,25 @@ import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Produced;
-import org.junit.Ignore;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.regex.Pattern;
 
 public class KafkaStreamsLiveTest {
     private String bootstrapServers = "localhost:9092";
     private Path stateDirectory;
 
     @Test
-    @Ignore("it needs to have kafka broker running on local")
+//    @Ignore("it needs to have kafka broker running on local")
     public void shouldTestKafkaStreams() throws InterruptedException {
         // given
-        String inputTopic = "inputTopic";
+        String  inputTopic = "inputTopic";
 
         Properties streamsConfiguration = new Properties();
         streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-live-test");
@@ -57,23 +56,36 @@ public class KafkaStreamsLiveTest {
         Pattern pattern = Pattern.compile("\\W+", Pattern.UNICODE_CHARACTER_CLASS);
 
         KTable<String, Long> wordCounts = textLines.flatMapValues(value -> Arrays.asList(pattern.split(value.toLowerCase())))
-            .groupBy((key, word) -> word)
-            .count();
+                                                   .groupBy((key, word) -> word)  // count words
+                                                   .count();
+
+        KTable<String, Long> alphaCounts = textLines.flatMapValues(value -> Arrays.asList(pattern.split(value.toLowerCase())))
+                                                   .groupBy((key, word) -> word.substring(0,1).toLowerCase())       // count words starting with letters
+                                                   .count();
+
+//        wordCounts.toStream()
+//                  .foreach((word, count) -> System.out.println("word: " + word + " -> " + count));
+
+//        alphaCounts.toStream()
+//                  .foreach((letter, count) -> System.out.printf("%n%d words start with letter: %s", count, letter ));
+
+        String wordCountTopic = "wordCountTopic";
 
         wordCounts.toStream()
-            .foreach((word, count) -> System.out.println("word: " + word + " -> " + count));
+                  .to(wordCountTopic, Produced.with(Serdes.String(), Serdes.Long()));
 
-        String outputTopic = "outputTopic";
+        String letterCountTopic = "letterCountTopic";
 
-        wordCounts.toStream()
-            .to(outputTopic, Produced.with(Serdes.String(), Serdes.Long()));
+        alphaCounts.toStream()
+                  .to(letterCountTopic, Produced.with(Serdes.String(), Serdes.Long()));
 
         final Topology topology = builder.build();
-        KafkaStreams streams = new KafkaStreams(topology, streamsConfiguration);
-        streams.start();
+        //System.out.println("topology: " + topology.describe());
 
-        // then
-        Thread.sleep(30000);
-        streams.close();
+        final int RUN_FOR_THIS_MANY_SECONDS = 60;
+        try (KafkaStreams streams = new KafkaStreams(topology, streamsConfiguration);) {
+            streams.start();
+            Thread.sleep(RUN_FOR_THIS_MANY_SECONDS * 1000);
+        }
     }
 }
